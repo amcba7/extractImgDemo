bcd4094f2b0b8b07bb0a0c110fcc457992a31d00
amcba7 <amcba7@gmail.com>
2025-10-02 18:30:59 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const navBar = document.querySelector('.header-nav');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        navBar.classList.add('sticky');
+    } else {
+        navBar.classList.remove('sticky');
+    }
+});
+
+const hmburgur = document.querySelector('.hamburgur');
+const mobileNav = document.querySelector('.mobile-nav');
+hmburgur.addEventListener('click',()=> {
+    // mobileNav.style.display = 'none';
+   if (mobileNav.style.display == 'none' || mobileNav.style.display == '') {
+        mobileNav.style.display = 'flex';
+   } else {
+    mobileNav.style.display = 'none';
+   }
+})
