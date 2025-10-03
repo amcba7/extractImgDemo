01fead0cc348ed223f12ff307d45395ddde30021
amcba7 <amcba7@gmail.com>
2025-10-03 09:09:32 +0530

    udpated nav

--- js/script.js
+++ js/script.js
@@ -11,9 +11,10 @@ const hmburgur = document.querySelector('.hamburgur');
 const mobileNav = document.querySelector('.mobile-nav');
 hmburgur.addEventListener('click',()=> {
     // mobileNav.style.display = 'none';
-   if (mobileNav.style.display == 'none' || mobileNav.style.display == '') {
-        mobileNav.style.display = 'flex';
-   } else {
-    mobileNav.style.display = 'none';
-   }
+//    if (mobileNav.style.display == 'none' || mobileNav.style.display == '') {
+//         mobileNav.style.display = 'flex';
+//    } else {
+//     mobileNav.style.display = 'none';
+//    }
+   mobileNav.classList.toggle("active");
 })
